eb2639f38994dc789350be9e9be0eba40ef40ff6
Shenol Shengov <shenol@Shenols-MacBook-Pro.local>
2025-12-26 15:23:09 +0200

    initial commit

---
+++ src/main/java/shengov/bg/pizzza_management_app/PizzzaManagementAppApplication.java
@@ -0,0 +1,13 @@
+package shengov.bg.pizzza_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzzaManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzzaManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shengov/bg/pizzza_management_app/PizzzaManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package shengov.bg.pizzza_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzzaManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
