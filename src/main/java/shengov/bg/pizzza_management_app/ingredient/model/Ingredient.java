893869ce474f98ebeff493ad8590df9f319410b4
ShenolShengov <shenolshengov41@gmail.com>
2025-12-27 17:49:40 +0200

    feat: add ingreient entity

---
+++ src/main/java/shengov/bg/pizzza_management_app/ingredient/model/Ingredient.java
@@ -0,0 +1,22 @@
+package shengov.bg.pizzza_management_app.ingredient.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table("ingredients")
+@Getter
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    @Setter
+    private String name;
+
+}
