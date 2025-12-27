3aa08165d155c83455640def55334b64f16e820a
ShenolShengov <shenolshengov41@gmail.com>
2025-12-27 21:52:26 +0200

    feat: add exception handler for ingredients

--- src/main/java/shengov/bg/pizzza_management_app/ingredient/exception/IngredientAlreadyExistsException.java
+++ src/main/java/shengov/bg/pizzza_management_app/ingredient/exception/IngredientAlreadyExistsException.java
@@ -2,6 +2,10 @@ package shengov.bg.pizzza_management_app.ingredient.exception;
 
 import static shengov.bg.pizzza_management_app.ingredient.constant.IngredientConstants.INGREDIENT_ALREADY_EXIST_MESSAGE;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class IngredientAlreadyExistsException extends RuntimeException {
 
   public IngredientAlreadyExistsException(String name) {
