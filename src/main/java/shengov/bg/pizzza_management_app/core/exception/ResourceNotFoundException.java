5f7438bd51dea20d55dd8da80872214649e0383c
ShenolShengov <shenolshengov41@gmail.com>
2025-12-28 13:04:28 +0200

    feat: add resource not found exception

---
+++ src/main/java/shengov/bg/pizzza_management_app/core/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package shengov.bg.pizzza_management_app.core.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+    }
+}
