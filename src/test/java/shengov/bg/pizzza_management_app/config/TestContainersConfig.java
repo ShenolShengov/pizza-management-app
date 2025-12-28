95ddbc4e6903f511784ea39e3c34a1ca96327617
ShenolShengov <shenolshengov41@gmail.com>
2025-12-28 18:29:32 +0200

    feat: set up test containers

---
+++ src/test/java/shengov/bg/pizzza_management_app/config/TestContainersConfig.java
@@ -0,0 +1,19 @@
+package shengov.bg.pizzza_management_app.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+@Testcontainers
+public class TestContainersConfig {
+
+  @Bean
+  @ServiceConnection("postgres")
+  public PostgreSQLContainer<?> postgreSQLContainer() {
+    return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+  }
+}
