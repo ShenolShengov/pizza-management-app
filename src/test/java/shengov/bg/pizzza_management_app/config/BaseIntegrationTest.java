6747c7fa937c959306f3915e3e7d540c0f549d14
ShenolShengov <shenolshengov41@gmail.com>
2025-12-31 15:52:25 +0200

    feat: enable method security

--- src/test/java/shengov/bg/pizzza_management_app/config/BaseIntegrationTest.java
+++ src/test/java/shengov/bg/pizzza_management_app/config/BaseIntegrationTest.java
@@ -1,12 +1,12 @@
 package shengov.bg.pizzza_management_app.config;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureWebMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Import({TestContainersConfig.class})
-@AutoConfigureWebMvc
+@AutoConfigureMockMvc
 public abstract class BaseIntegrationTest {}
