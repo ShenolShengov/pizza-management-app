4f0497e6b3f7c55d5bbaf771c73de0ff31940b88
ShenolShengov <shenolshengov41@gmail.com>
2026-01-20 17:15:40 +0200

    feat: expose get all endpoint

--- src/main/java/shengov/bg/pizzza_management_app/size/controller/SizeController.java
+++ src/main/java/shengov/bg/pizzza_management_app/size/controller/SizeController.java
@@ -1,13 +1,18 @@
 package shengov.bg.pizzza_management_app.size.controller;
 
 import jakarta.validation.Valid;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import shengov.bg.pizzza_management_app.size.dto.SizeRequest;
 import shengov.bg.pizzza_management_app.size.dto.SizeResponse;
 import shengov.bg.pizzza_management_app.size.service.SizeService;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/sizes")
@@ -24,5 +29,11 @@ public class SizeController {
   @GetMapping("/{id}")
   public ResponseEntity<SizeResponse> get(@PathVariable UUID id) {
     return ResponseEntity.ok(sizeService.getById(id));
+  }
+
+  @GetMapping
+    public ResponseEntity<PagedModel<SizeResponse>> getAll(@PageableDefault Pageable pageable) {
+      Page<SizeResponse> response = sizeService.getAll(pageable);
+      return ResponseEntity.ok(new PagedModel<>(response));
   }
 }
