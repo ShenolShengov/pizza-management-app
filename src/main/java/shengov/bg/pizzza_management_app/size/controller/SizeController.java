e57c8fa7031161a3b56018f54dfa6318b28c7e76
ShenolShengov <shenolshengov41@gmail.com>
2026-01-20 16:22:45 +0200

    feat: expose getById

--- src/main/java/shengov/bg/pizzza_management_app/size/controller/SizeController.java
+++ src/main/java/shengov/bg/pizzza_management_app/size/controller/SizeController.java
@@ -1,12 +1,10 @@
 package shengov.bg.pizzza_management_app.size.controller;
 
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shengov.bg.pizzza_management_app.size.dto.SizeRequest;
 import shengov.bg.pizzza_management_app.size.dto.SizeResponse;
 import shengov.bg.pizzza_management_app.size.service.SizeService;
@@ -21,5 +19,10 @@ public class SizeController {
   @PostMapping
   public ResponseEntity<SizeResponse> create(@RequestBody @Valid SizeRequest request) {
     return ResponseEntity.ok(sizeService.create(request));
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<SizeResponse> get(@PathVariable UUID id) {
+    return ResponseEntity.ok(sizeService.getById(id));
   }
 }
