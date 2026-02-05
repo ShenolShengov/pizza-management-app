4a6e90b34cba2948e028d5d13538db6daa0bf6a0
ShenolShengov <shenolshengov41@gmail.com>
2026-02-06 00:01:47 +0200

    feat: add pizza constants

---
+++ src/main/java/shengov/bg/pizzza_management_app/pizza/constants/PizzaConstants.java
@@ -0,0 +1,32 @@
+package shengov.bg.pizzza_management_app.pizza.constants;
+
+public final class PizzaConstants {
+
+    private PizzaConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String NAME_REQUIRED =
+            "Pizza name is required";
+
+    public static final String NAME_SIZE =
+            "Pizza name must be between 3 and 40 characters";
+
+    public static final String DESCRIPTION_SIZE =
+            "Description cannot exceed 200 characters";
+
+    public static final String INGREDIENTS_REQUIRED =
+            "Pizza must have at least one ingredient";
+
+    public static final String SIZES_REQUIRED =
+            "Pizza must have at least one size";
+
+    public static final String SIZE_ID_REQUIRED =
+            "Size ID is required";
+
+    public static final String PRICE_REQUIRED =
+            "Price is required";
+
+    public static final String PRICE_POSITIVE =
+            "Price must be greater than 0";
+}
