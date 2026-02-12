4b0eb64f46c9014cdf88f4e4b2d1d95ac000a63d
ShenolShengov <shenolshengov41@gmail.com>
2026-02-12 08:07:16 +0200

    feat-pizza: expose get all endpoint

--- src/main/java/shengov/bg/pizzza_management_app/pizza/controller/PizzaController.java
+++ src/main/java/shengov/bg/pizzza_management_app/pizza/controller/PizzaController.java
@@ -1,6 +1,11 @@
 package shengov.bg.pizzza_management_app.pizza.controller;
 
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,17 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 import shengov.bg.pizzza_management_app.pizza.dto.PizzaResponse;
 import shengov.bg.pizzza_management_app.pizza.service.PizzaService;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/api/pizzas")
 @RequiredArgsConstructor
 public class PizzaController {
 
-    private final PizzaService pizzaService;
+  private final PizzaService pizzaService;
 
-    @GetMapping("{id}")
-    public ResponseEntity<PizzaResponse> byId(@PathVariable UUID id) {
-        return ResponseEntity.ok(pizzaService.getById(id));
-    }
+  @GetMapping("{id}")
+  public ResponseEntity<PizzaResponse> byId(@PathVariable UUID id) {
+    return ResponseEntity.ok(pizzaService.getById(id));
+  }
+
+  @GetMapping
+  public ResponseEntity<PagedModel<PizzaResponse>> all(@PageableDefault Pageable pageable) {
+    Page<PizzaResponse> response = pizzaService.getAll(pageable);
+    return ResponseEntity.ok(new PagedModel<>(response));
+  }
 }
